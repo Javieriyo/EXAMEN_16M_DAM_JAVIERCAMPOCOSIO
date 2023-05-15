51c99e9f5f2dd1b6695f07d6b1489fa24f878e03
Javier <Javier@DESKTOP-7O8P9C8>
2023-05-15 09:03:34 +0200

    inicio

---
+++ src/A/B.java
@@ -0,0 +1,17 @@
+package A;
+
+import java.util.Scanner;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("buenos dias");
+		
+		int i;
+		System.out.println("introduce un numero");
+		Scanner ent = new Scanner(System.in);
+		i=ent.nextInt();
+
+	}
+
+}
